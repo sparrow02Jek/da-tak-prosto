b6339f0149453cd8d7f117101955fa1b4369b571
sparrow02Jek <82744411+sparrow02Jek@users.noreply.github.com>
2021-05-11 22:52:21 +0500

    Add files via upload

---
+++ Assets/Mario/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+
+    [SerializeField] private MeshFilter meshFilter;
+   
+    private void Awake()
+    {
+       meshFilter = GetComponent<MeshFilter>();
+        
+    }
+
+    private void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        print("ns kj[");
+    }
+    //
+    private void OnValidate()
+    {
+        if (meshFilter == null)
+            meshFilter = GetComponent<MeshFilter>();
+        SetMesh();
+    }
+    private void SetMesh()
+    {
+        print("SetMesh");
+        meshFilter.mesh = meshFilter.sharedMesh;
+    }
+
+
+}
