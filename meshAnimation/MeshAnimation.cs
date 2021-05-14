770a43694ed839f3b3687f8f5b132dd3b2c4859e
sparrow02Jek <alixanadilbaev02@gmail.com>
2021-05-14 12:11:31 +0500

    MeshAnimationControll
    this Git  for Controll our mesh animtion

---
+++ meshAnimation/MeshAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "meshAnimation")]
+public class MeshAnimation : ScriptableObject
+{
+    [SerializeField] private MeshAnimationType meshAnimationType = MeshAnimationType.Cycling;
+
+    [SerializeField] private string name;
+    [SerializeField] private MeshAnimPart[] meshAnim;
+
+    public MeshAnimPart[] MeshAnimParts => meshAnim;
+    public string Name => name;
+
+    private MeshAnimationEngine _meshAnimationLinear;
+    private MeshAnimationEngine _meshAnimationCycling;
+
+    private MeshFilter _meshFilter;
+
+    private MeshAnimationEngine _currentAnimationEngine;
+
+    public virtual void Initialize(MeshFilter meshFilter)
+    {
+        _meshFilter = meshFilter;
+        
+        _meshAnimationLinear = new MeshAnimationLinear(this, _meshFilter);
+        _meshAnimationCycling = new MeshAnimationCycling(this,_meshFilter);
+
+    }
+
+    public IEnumerator RunAnimation()
+    {
+        return Run();
+    }
+
+    private IEnumerator Run()
+    {
+        switch (meshAnimationType) 
+        {
+            case (MeshAnimationType.Cycling):
+                _currentAnimationEngine = _meshAnimationCycling;
+                break;
+            case (MeshAnimationType.Linear):
+                _currentAnimationEngine = _meshAnimationLinear;
+                break;
+        }
+        return _currentAnimationEngine.Run();
+    }
+}
---
+++ meshAnimation/MeshAnimator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshAnimator : MonoBehaviour
+{
+    [SerializeField] private List<MeshAnimation> meshAnimations;
+
+    private MeshFilter _meshFilter;
+
+    private void Start()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        Initialize();
+    }
+
+    public void Play(string name)
+    {
+        MeshAnimation animation = FindAnimation(name);
+
+        if (animation)
+            StartCoroutine(animation.RunAnimation());
+        else
+            throw new Exception($"Animation: {name} is not Found");
+    }
+
+    public MeshAnimation FindAnimation(string name)
+    {
+        MeshAnimation animation = meshAnimations.Find(m => m.Name == name);
+
+        if (animation)
+            return animation;
+
+        return null;
+    }
+
+    private void Initialize()
+    {
+        for (int i = 0; i < meshAnimations.Count; i++)
+        {
+            meshAnimations[i] = Instantiate(meshAnimations[i]);
+            meshAnimations[i].Initialize(_meshFilter);
+        }
+    }
+}
